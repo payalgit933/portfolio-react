63dbc1352b2873e3201eca0d577f2e412f9f092f
payalgit933 <payalgit933@gmail.com>
2025-05-29 03:32:41 +0530

    Add files via upload

---
+++ src/components/Contact.jsx
@@ -0,0 +1,22 @@
+function Contact () {
+    return(
+        <div id="contact" style={{
+                    minHeight: "722px",
+                }} className="bg-sky-300">
+            <div className="logo p-8 bg-blue-200">
+                <h2 className="font-bold text-pink-500 text-4xl absolute text-blue-800">ℙ𝕒𝕪𝕒𝕝👩‍💻</h2>
+
+                <div className="float-right">
+                    <a href="#" className="m-3 p-2 text-blue-1000 font-bold">Home</a>
+                    <a href="#about" className="m-3 p-2 text-blue-1000 font-bold">About</a>
+                    <a href="#education" className="m-3 p-2 text-blue-1000 font-bold">Education</a>
+                    <a href="#skills" className="m-3 p-2 text-blue-1000 font-bold">Skills</a>
+                    <a href="#contact" className="m-3 p-2 text-blue-1000 font-bold">Contact</a>
+                </div>
+            </div>
+            <h1 className="about_body text-blue-1000 font-bold text-5xl">Contact 𝐌𝐞</h1>
+        </div>
+    )
+}
+
+export default Contact
